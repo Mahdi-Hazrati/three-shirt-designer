8b30254cd750560353614278d07a841f692e7aa8
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-08-27 12:59:28 +0430

    create some file

--- src/App/App.js
+++ src/App/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <h1 className="head-text">Three Shirt Designer initial App</h1>
+            <h1 className="head-text">App</h1>
         </div>
     );
 }
