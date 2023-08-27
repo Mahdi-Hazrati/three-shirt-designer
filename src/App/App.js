454dd515b7e105e18a42260efd7be80ac8cc986b
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-08-27 10:27:01 +0430

    add inital code

--- src/App/App.js
+++ src/App/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <h1 className="text-3xl font-bold underline hover:border-b-black">Hello world!</h1>
         </div>
     );
 }
