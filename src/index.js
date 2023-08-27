30fc07e411c3c6b77c4af2510887db9c7dbd2a23
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-08-27 09:36:49 +0430

    add react-icons & fix root element id in index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('web-app'));
 root.render(
   <React.StrictMode>
     <App />
