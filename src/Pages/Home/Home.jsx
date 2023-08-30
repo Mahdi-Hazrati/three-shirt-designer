0fa6c8554a5f662a1b2715a6ab472ebe393eefbc
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-08-30 13:26:39 +0430

    Home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
   return (
     <AnimatePresence>
       {snap.intro && (
-        <motion.section className="home" {...slideAnimation("up")}>
+        <motion.section className="home" {...slideAnimation("left")}>
           <motion.header {...slideAnimation("down")}>
             <img
               className='w-8 h-8 object-contain'
