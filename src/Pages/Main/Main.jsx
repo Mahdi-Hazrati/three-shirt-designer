7c0b1010d7596e4f2fcf12a823390698cb019b47
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-08-31 17:21:48 +0430

    add tab menu

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -7,15 +7,33 @@ import { downloadCanvasToImage, reader } from "../../Config/helpers"
 import { EditorTabs, DecalTypes, FilterTabs } from "../../Config/constants"
 import { fadeAnimation, slideAnimation } from "../../Config/motion"
 import { AiOutlineDownload } from "react-icons/ai"
-import {CustomButton,AiPicker,ColorPicker,FilePicker,Tab,Canvas} from "../../Components/index"
+import { CustomButton, AiPicker, ColorPicker, FilePicker, Tab, Canvas }
+  from "../../Components/index"
 import "./Main.css"
 
 const Main = () => {
   const snap = useSnapshot(state)
   return (
-    <div>
-      Wellcome to Home
-    </div>
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          {/* Right Menu */}
+          <motion.div key="custom" className='absolute top-0 left-0 z-10' {...slideAnimation("left")}>
+            <div className="flex item-center min-h-screen">
+              <div className="editortabs-container tabs">
+                {EditorTabs.map((tab) => (
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    onClick={() => { }}
+                  />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
   )
 }
 
