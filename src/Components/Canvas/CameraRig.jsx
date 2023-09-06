c3b1040f427fea117debad49ad7447cfb0c006e6
Mahdi Hazrati <127740921+Mahdi-Hazrati@users.noreply.github.com>
2023-09-06 07:20:22 +0000

    useframe config

--- src/Components/Canvas/CameraRig.jsx
+++ src/Components/Canvas/CameraRig.jsx
@@ -1,17 +1,29 @@
-import React, {useRef} from 'react'
+import React, { useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 import { easing } from 'maath'
 import { useSnapshot } from 'valtio'
 import state from '../../Config/state'
 
-const CameraRig = ({children}) => {
+const CameraRig = ({ children }) => {
   const snap = useSnapshot(state)
   const group = useRef()
+
+  useFrame((state, delta) => {
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5],
+      0.25,
+      delta,
+
+
+    )
+  })
+  
   return (
     <group ref={group}>
       {children}
     </group>
-    
+
   )
 }
 
