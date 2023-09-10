889700b07ae438e15ca8eda53f9a2bf8402f7e4e
Mahdi Hazrati <127740921+Mahdi-Hazrati@users.noreply.github.com>
2023-09-10 19:33:20 +0000

    add color picker component full

--- src/Components/ColorPicker/ColorPicker.jsx
+++ src/Components/ColorPicker/ColorPicker.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { SketchPicker } from "react-color"
+import { useSnapshot } from 'valtio'
+import state from "../../Config/state"
 
 const ColorPicker = () => {
+  const snap = useSnapshot(state)
   return (
-    <div>ColorPicker</div>
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+        color={snap.tshertColor}
+        disableAlpha
+        presetColors={[
+          "#ff0000", "#ff9900", "#e3d904", "#00ff00", "#00ccff", "#9900ff", "#ffffff", "#000000", "#ff66cc", "#33cc33"
+        ]}
+        onChange={(color) => { state.tshertColor = color.hex }}
+      />
+    </div>
   )
 }
 
--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -7,8 +7,7 @@ import { downloadCanvasToImage, reader } from "../../Config/helpers"
 import { EditorTabs, DecalTypes, FilterTabs } from "../../Config/constants"
 import { fadeAnimation, slideAnimation } from "../../Config/motion"
 import { AiOutlineDownload } from "react-icons/ai"
-import { CustomButton, AiPicker, ColorPicker, FilePicker, Tab, Canvas }
-  from "../../Components"
+import { CustomButton, AiPicker, ColorPicker, FilePicker, Tab, Canvas } from "../../Components"
 import "./Main.css"
 
 const Main = () => {
