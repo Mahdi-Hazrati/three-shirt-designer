2dc110d941ed5e064e436b3b60c5dcc098f29916
Mahdi Hazrati <127740921+Mahdi-Hazrati@users.noreply.github.com>
2023-09-10 10:23:55 +0000

    Tab styles done

--- src/Components/Tab/Tab.jsx
+++ src/Components/Tab/Tab.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import { useSnapshot } from 'valtio'
 import state from "../../Config/state"
 
-const Tab = ({tab, isFilter, isActive, onClick}) => {
+const Tab = ({ tab, isFilter, isActive, onClick }) => {
   const snap = useSnapshot(state)
+  const activeStyle = isFilter && isActive
+    ? { backgroundColor: snap.color, opacity: 0.5 }
+    : { backgroundColor: "transparent", opacity: 1 }
 
   return (
     <div
-    key={tab.name}
-    className={`tab-btn ${isFilter ? "rounded-full glassmorphism":"rounded-4"}`}
-    onClick={onClick}
-    >{tab.icon}
+      key={tab.name}
+      style={activeStyle}
+      className={`tab-btn ${isFilter ? "rounded-full glassmorphism" : "rounded-4"}`}
+      onClick={onClick}
+    >
+      <div className={`${isFilter ? "w-2/3 h-2/3":"w-11/12 h-11/12"} flex justify-center items-center`}>{tab.icon}</div>
+      
     </div>
   )
 }
