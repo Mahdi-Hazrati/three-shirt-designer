f60b0b074359bbf70d33d328e4a80407d055a7a0
Mahdi Hazrati <127740921+Mahdi-Hazrati@users.noreply.github.com>
2023-09-10 09:36:32 +0000

    add tab inital style

--- src/Components/Canvas/Backdrop.jsx
+++ src/Components/Canvas/Backdrop.jsx
@@ -14,8 +14,6 @@ const Backdrop = () => {
       scale={5}
       rotation={[Math.PI / 2, 0, 0]}
       position={[0, 0, -0.14]}
-      
-
     >
       <RandomizedLight
         amount={4}
--- src/Components/Tab/Tab.jsx
+++ src/Components/Tab/Tab.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useSnapshot } from 'valtio'
+import state from "../../Config/state"
 
-const Tab = ({tab}) => {
+const Tab = ({tab, isFilter, isActive, onClick}) => {
+  const snap = useSnapshot(state)
+
   return (
-    <div>{tab.icon}</div>
+    <div
+    key={tab.name}
+    className={`tab-btn ${isFilter ? "rounded-full glassmorphism":"rounded-4"}`}
+    onClick={onClick}
+    >{tab.icon}
+    </div>
   )
 }
 
