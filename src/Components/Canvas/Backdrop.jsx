a3e6f4a929ba57edcca990dc747a9d18e0f0ee2d
Mahdi Hazrati <127740921+Mahdi-Hazrati@users.noreply.github.com>
2023-09-10 08:32:19 +0000

    add backdrop shadow

--- src/Components/Canvas/Backdrop.jsx
+++ src/Components/Canvas/Backdrop.jsx
@@ -4,9 +4,33 @@ import { useFrame } from '@react-three/fiber'
 import { AccumulativeShadows, RandomizedLight } from '@react-three/drei'
 
 const Backdrop = () => {
+  const shadow = useRef()
   return (
-    <AccumulativeShadows position={[0, 0, 0]}>
-      <RandomizedLight amount={4} />
+    <AccumulativeShadows
+      ref={shadow}
+      temporal
+      frames={60}
+      alphaTest={0.2}
+      scale={5}
+      rotation={[Math.PI / 2, 0, 0]}
+      position={[0, 0, -0.14]}
+      
+
+    >
+      <RandomizedLight
+        amount={4}
+        radius={9}
+        intensity={0.5}
+        ambient={0.2}
+        position={[5, 5, -10]}
+      />
+      <RandomizedLight
+        amount={4}
+        radius={5}
+        intensity={0.2}
+        ambient={0.5}
+        position={[-5, 5, -9]}
+      />
     </AccumulativeShadows>
   )
 }
